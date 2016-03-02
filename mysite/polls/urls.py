edeb36d58dffc3d7f2015021798e845fa8eec5fb
Joshua Liu <liuchaozhenyu@gmail.com>
2016-03-02 15:12:02 +0800

    using generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,7 @@
 # @Author: Joshua Liu
 # @Email: liuchaozhen@neusoft.com
 # @Create Date: 2016-03-02 09:03:39
-# @Last Modified: 2016-03-02 12:03:51
+# @Last Modified: 2016-03-02 15:03:34
 # @Description:
 from django.conf.urls import url
 from . import views
@@ -12,11 +12,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
         # 例如： /polls/
-        url(r'^$', views.index, name='index'),
+        url(r'^$', views.IndexView.as_view(), name='index'),
         # 例如： /polls/5/
-        url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+        url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
         # 例如： /polls/5/results/
-        url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+        url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
         # 例如： /polls/5/vote/
         url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,29 +4,30 @@
 # @Author: Joshua Liu
 # @Email: liuchaozhenyu@gmail.com
 # @Create Date: 2016-03-02 09:03:48
-# @Last Modified: 2016-03-02 14:03:54
+# @Last Modified: 2016-03-02 15:03:08
 # @Description:
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-            'latest_question_list': latest_question_list,
-        }
-    return render(request, 'polls/index.html', context)
+from .models import Question, Choice
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    def get_queryset(self):
+        ''' 返回最近的5个问题 '''
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
