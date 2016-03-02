f8e1d8ee7c0ba713bf80aab2af118c90ec222c07
Joshua Liu <liuchaozhenyu@gmail.com>
2016-03-02 10:50:59 +0800

    404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,12 +4,13 @@
 # @Author: Joshua Liu
 # @Email: liuchaozhenyu@gmail.com
 # @Create Date: 2016-03-02 09:03:48
-# @Last Modified: 2016-03-02 10:03:47
+# @Last Modified: 2016-03-02 10:03:08
 # @Description:
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -20,7 +21,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("请看问题 %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("妈的，没有这个问题了！")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "问题答案 %s."
